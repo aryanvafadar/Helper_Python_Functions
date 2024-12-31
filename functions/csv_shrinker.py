784a049e5f1b119913d2a8d0b1cd84a0e6e6255c
frostbitefutures <frostbitefutures@gmail.com>
2024-12-31 09:28:40 -0500

    Made the csv shrinker and string reverser

---
+++ functions/csv_shrinker.py
@@ -0,0 +1,30 @@
+"""
+Takes a CSV File, converts it to a dataframe and converts it back to a csv file
+"""
+import pathlib
+import pandas as pd
+
+
+
+def main():
+    
+    # convert csv to frame
+    frame = pd.read_csv(filepath_or_buffer=input_file, nrows=chunk_size)
+    
+    # convert back to csv
+    frame.to_csv(output_file, index=False)
+
+
+if __name__ == "__main__":
+    
+    current_directory = pathlib.Path(__file__).parent.parent
+    
+    try:
+        input_file = input("File Path for Input File: ")
+        chunk_size = int(input("Enter Number of Rows for New File: "))
+        output_file = str(current_directory.joinpath('data_files', 'output_files')) + '/new_' + input_file.split(sep='/')[-1]
+        
+        main()
+    
+    except Exception as e:
+        print(f'{e}')
---
+++ functions/string_reverser.py
@@ -0,0 +1,22 @@
+"""
+Takes any number of words and reverses it.
+"""
+
+
+def main():
+    
+    reversed_text = []
+    
+    for letter in reversed(input_text):
+        reversed_text.append(letter)
+    
+    for letter in reversed_text:
+        print(letter, end='')
+
+
+
+if __name__ == "__main__":
+    
+    input_text = input("Enter Text or Sentence to Reverse: ")
+    
+    main()
